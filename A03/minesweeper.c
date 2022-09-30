12daccaf3aacadc47756c9deef42b9ee2dc0f4ee
Tsenguun Chinzorig <tchinzorig@goldengate.cs.brynmawr.edu>
2022-09-29 22:51:39 -0400

    A03 complete

--- A03/minesweeper.c
+++ A03/minesweeper.c
@@ -1,3 +1,135 @@
-int main() {
+#include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
+
+char random_c (double value) {
+
+  
+  char status = 'o';
+
+  double whole = value * 100; 
+  int number = (rand() % 100);
+  if (number < whole) {
+    status = 'X';
+  } if (number > whole) {
+    status = '.';
+  }
+  return status;
+}
+
+
+void x_generation (char **storage, int m, int n) {
+  int count_of_x = 0;
+ 
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if (storage[i][j] != 'X') {
+        if (i != 0 && storage[i-1][j] == 'X') {
+          count_of_x++;
+        } if (j != 0 && storage[i][j-1] == 'X') {
+          count_of_x++;
+        } if (j != m - 1 && storage[i][j+1] == 'X') {
+          count_of_x++;
+        } if (i != n - 1 && storage[i+1][j] == 'X') {
+          count_of_x++;
+        } if (i != n -1 && j != m - 1 && storage[i+1][j+1] == 'X') {
+          count_of_x++;
+        } if (i != n - 1 && j != 0 && storage[i+1][j-1] == 'X') {
+          count_of_x++;
+        } if ( i != 0 && j != m - 1 && storage[i-1][j+1] == 'X') {
+          count_of_x++;
+        } if ( i != 0 && j != 0 && storage[i-1][j-1] == 'X') {
+          count_of_x++;
+        } 
+      
+
+      if (count_of_x == 8) {
+        storage[i][j] = '8';
+      } else if (count_of_x == 7) {
+        storage[i][j] = '7';
+      } else if (count_of_x == 6) {
+        storage[i][j] = '6';
+      } else if (count_of_x == 5) {
+        storage[i][j] = '5';
+      } else if (count_of_x == 4) {
+        storage[i][j] = '4';
+      } else if (count_of_x == 3) {
+        storage[i][j] = '3';
+      } else if (count_of_x == 2) {
+        storage[i][j] = '2';
+      } else if (count_of_x == 1) {
+        storage[i][j] = '1';
+      } else if (count_of_x == 0) {
+        storage[i][j] = '0';
+      }
+      }
+      count_of_x = 0;
+    }
+  }
+
+ 
+
+}
+int main(int argc, char *argv[]) {
+srand(time(0));
+
+
+char *a = argv[1];
+int m = atoi(a);
+
+char *b = argv[2];
+int n = atoi(b);
+
+char *c = argv[3];
+double p = atof(c);
+
+int number_elements = m * n;
+
+
+char **storage = (char**) calloc(number_elements, sizeof(char*));
+
+for (int i = 0; i < number_elements; i++) {
+  storage[i] = (char*) calloc(number_elements, sizeof(char));
+}
+
+for (int i = 0; i<m; i++) {
+  for (int j = 0; j < n; j++) {
+    storage[i][j] = random_c(p);
+  }
+}
+
+//printing the 2d array with mines
+printf("\n");
+
+for (int i = 0; i< m; i++) {
+  for (int j = 0; j < n; j++) {
+    printf("%c",storage[i][j]);
+  }
+  printf("\n");
+}
+
+printf("\n");
+
+x_generation(storage, m, n);
+
+//printing the 2d array after revealing the N of mines
+for (int i = 0; i< m; i++) {
+  for (int j = 0; j < n; j++) {
+    printf("%c",storage[i][j]);
+  }
+  printf("\n");
+}
+
+
+
+
+//FREE STORAGE
+for (int j = 0; j < number_elements; j++) {
+  free(storage[j]);
+}
+free(storage);
+
+
   return 0;
 }
+
--- A03/sorted_restaurants.c
+++ A03/sorted_restaurants.c
@@ -1,3 +1,140 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+struct Restaurants {
+    char name[50];
+    int start_time;
+    int end_time;
+    float rating;
+    int key; 
+    struct Restaurants *next; 
+}; 
+
+struct Restaurants *head = NULL;
+//struct Restaurants *current = NULL; 
+
+struct Restaurants* insert_sort (char name[], int start_time, int end_time, float rating, struct Restaurants* head) {
+  struct Restaurants *data = (struct Restaurants*) malloc (sizeof(struct Restaurants));
+  if (data == NULL) {
+    printf("ERROR: Out of space!\n");
+    exit(1);
+  }
+
+  memset (data -> name, 0, 50);
+  strcpy (data -> name, name);
+  data -> start_time = start_time;
+  data -> end_time = end_time;
+  data -> rating = rating; 
+  data -> next = NULL;
+
+  //sort
+  struct Restaurants *tmp;
+  struct Restaurants *tmp1; 
+  
+
+  tmp = head; 
+
+  if (head == NULL) {
+      return data;
+    }
+
+  // case 1 
+  if (tmp -> rating < data -> rating) {
+      data -> next = tmp; 
+      return data;
+    }
+
+  // case 2 
+  while (tmp->next != NULL && tmp -> rating > data -> rating) {
+
+    tmp1 = tmp; 
+    tmp = tmp -> next;
+    
+  }  
+
+    if (tmp -> next == NULL) {
+      tmp -> next = data;
+    } else {
+      tmp1 -> next = data; 
+      data -> next = tmp;
+      
+    }
+
+    return head;
+}
+  
+
+  
+//clear all the values
+void clear (struct Restaurants* head) {
+  struct Restaurants *tmp = head;
+  while(head -> next != NULL) {
+    tmp = tmp -> next;
+    free(head);
+    head = tmp;
+  }
+  free(tmp);
+}
+
+
+
+void print(struct Restaurants* list) {
+  int i = 1;
+  for (struct Restaurants* data = list; data != NULL; data = data -> next) {
+    
+    printf ("\n%d) %-32sOPEN: %d:00 \tCLOSE: %d:00 \tRATING: %.2f\n",
+                i++, data -> name, data -> start_time, data -> end_time, data -> rating);
+
+  }
+}
+
+
 int main() {
-  return 0;
+
+    struct Restaurants *restaurant_data = NULL;
+
+
+    char name[32]; 
+    int start_time;
+    int end_time;
+    float rating;
+
+    int number_of_restaurants;
+    printf("Enter the number of restaurants: "); 
+    scanf("%d", &number_of_restaurants);
+
+
+    for (int i = 0; i < number_of_restaurants; i++) { 
+      printf("Enter restaurant name: "); 
+      scanf(" %[^\n]%*c", (name));
+
+      printf("Enter start time: ");
+      scanf(" %d%*c", &(start_time));
+
+      printf("Enter closing time: "); 
+      scanf(" %d%*c", &(end_time));
+
+      printf("Enter rating: ");
+      scanf(" %f%*c", &(rating));
+
+      restaurant_data = insert_sort(name, start_time, end_time, rating, restaurant_data);
+   
+    }
+
+    
+    
+
+    printf("\n");
+    printf("\tWelcome to the Restaurant Directory!");
+    printf("\n");
+
+    print(restaurant_data);
+
+
+    clear(restaurant_data);
+
+
+  
 }
