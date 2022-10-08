2321724fc9cb5b274220c3a81b8a0486f866a9ae
Tsenguun1129 <71144764+Tsenguun1129@users.noreply.github.com>
2022-10-08 15:19:15 -0400

    Update test_ppm.c

--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -4,6 +4,19 @@
 
 int main() {
   // load feep-raw and print out the grid of pixels
+  int w, h; 
+  struct ppm_pixel** example;
+  example = read_ppm_2d ("feep-raw.ppm", &w, &h);
+  printf("%s", "Opening file: feep-raw.ppm\n");
+  for (int i = 0; i<h; i++) {
+    for (int j = 0; j<w; j++) {
+      struct ppm_pixel a = example[i][j];
+      printf("(%u, %u, %u) ", a.red, a.green, a.blue);
+    }
+    printf("\n");
+  }
+  free(example);
+  
   return 0;
 }
 
