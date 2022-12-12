a70d947cc60f1a891f8166b774f180e747ff1212
Tsenguun1129 <71144764+Tsenguun1129@users.noreply.github.com>
2022-12-11 21:52:08 -0500

    Update single_mandelbrot.c

--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -7,13 +7,12 @@
 #include "write_ppm.h"
 
 int main(int argc, char* argv[]) {
-  int size = 480;
+  int size = 400;
   float xmin = -2.0;
   float xmax = 0.47;
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
-
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
     switch (opt) {
@@ -29,10 +28,69 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
-  srand(time(0));
 
-  // compute image
 
+
+//just wanted to use the clock for fun
+srand(clock());
+
+//pallete has the same number of max iterations per instruction. Using calloc to set everything as 0
+struct ppm_pixel *palette = calloc(maxIterations, sizeof(struct ppm_pixel));
+for(int i=0; i<maxIterations; i++){
+  palette[i].red = rand()% 255;
+  palette[i].green = rand()% 255;
+  palette[i].blue = rand()% 255;
+}
+
+
+
+double time_check;
+struct timeval a, b; 
+int current = time(0);
+gettimeofday(&a, NULL);
+char fp[100];
+
+struct ppm_pixel *pixels = calloc(size * size, sizeof(struct ppm_pixel));
+
+//algorithm given for A09
+for(int i = 0; i<size; i++){
+  for(int j = 0; j<size; j++){
+    float xfrac = (float) (j/size);
+    float yfrac = (float) (i/size);
+    float x0 = xmin + xfrac * (xmax - xmin);
+    float y0 = ymin + yfrac * (ymax - ymin);
+  
+    float x = 0.0;
+    float y = 0.0;
+    int iter = 0;
+    while (iter < maxIterations && x*x + y*y < 2*2){
+      float xtmp = x*x - y*y + x0;
+      y = 2*x*y + y0;
+      x = xtmp;
+      iter++;
+    }
+    if(iter < maxIterations){
+      pixels[i*size + j] = palette[iter];
+    }
+    else{
+      pixels[i*size + j].red = 0;
+      pixels[i*size + j].green = 0;
+      pixels[i*size + j].blue = 0;
+    }
+  }
+}
+
+//class code
+gettimeofday(&b, NULL);
+time_check = b.tv_sec - a.tv_sec + (b.tv_usec-a.tv_usec)/1.e6;
+printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size, time_check);
+
+//writing to a file
+  sprintf(fp, "mandelbrot-%d-%d.ppm", size, current);
+  printf("Writing file: %s\n", fp);
+  write_ppm(fp, pixels, size, size);
+
+//free
+  free(pixels);
+  free(palette);
 }
